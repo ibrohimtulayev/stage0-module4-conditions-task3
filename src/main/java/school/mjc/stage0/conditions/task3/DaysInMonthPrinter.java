3be0740cace95ead31e9c231819822ee8a3c83e1
tulayevibrohim <tulayevibrohimjon@gmail.com>
2023-02-12 19:26:02 +0500

    hiu

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,32 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month==28){
+            System.out.println("28 days");
+        } if (month==31){
+            System.out.println("31 days");
+        } if (month==30){
+            System.out.println("30 days");
+        } if (month==31){
+            System.out.println("31 days");
+        } if (month==30){
+            System.out.println("30 days");
+        } if (month==31){
+            System.out.println("31 days");
+        } if (month==30){
+            System.out.println("30 days");
+        } if (month==31){
+            System.out.println("31 days");
+        } if (month==30){
+            System.out.println("30 days");
+        } if (month==31){
+            System.out.println("31 days");
+        } if (month==30){
+            System.out.println("30 days");
+        } if (month==31){
+            System.out.println("31 days");
+        }else {
+            System.out.println("wrong number!");
+        }
     }
 }
